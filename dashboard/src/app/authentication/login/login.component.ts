aee2b1bba72bb343f1aa0716a7c1b1e4f46f1c75
Icelero1@ <purnima_kumari@winnow.co>
2021-11-13 21:29:51 +0530

    fixes

--- dashboard/src/app/authentication/login/login.component.ts
+++ dashboard/src/app/authentication/login/login.component.ts
@@ -11,16 +11,46 @@ import { Auth } from 'aws-amplify';
 })
 export class LoginComponent implements OnInit {
 
-
+  error_message = '';
+  show_error_message = false;
   loginForm: FormGroup;
 
-  submitForm(): void {
-      for (const i in this.loginForm.controls) {
-          this.loginForm.controls[ i ].markAsDirty();
-          this.loginForm.controls[ i ].updateValueAndValidity();
-      }
+  listOfUsers = [
+    { username: "admin@gmail.com", password: "123456" },
+    { username: "kumudini@gmail.com", password: "123456" },
+    { username: "user@gmail.com", password: "123456" }
+  ]
 
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]]
+    });
+  }
+
+  submitForm(): void {
+    this.show_error_message = false;
+    for (const i in this.loginForm.controls) {
+      this.loginForm.controls[i].markAsDirty();
+      this.loginForm.controls[i].updateValueAndValidity();
+    }
+
+    console.log(this.loginForm)
+
+    if (this.loginForm.value.userName == 'admin@gmail.com' && this.loginForm.value.password == "123456") {
       this.router.navigate(['/dashboard/home']);
+    }else if (this.loginForm.value.userName == 'user@gmail.com' && this.loginForm.value.password == "123456") {
+      this.router.navigate(['/dashboard/home']);
+    }
+    else if (this.loginForm.value.userName == 'demo@gmail.com' && this.loginForm.value.password == "123456") {
+      this.router.navigate(['/dashboard/home']);
+    }
+    else {
+      this.error_message = 'User not authenticated!';
+      this.show_error_message = true;
+    }
+
+
   }
 
   constructor(
@@ -28,13 +58,8 @@ export class LoginComponent implements OnInit {
     private router: Router,
     private auth: AuthService,
     private route: ActivatedRoute
-    ) {
+  ) {
   }
 
-  ngOnInit(): void {
-      this.loginForm = this.fb.group({
-          userName: [ null, [ Validators.required ] ],
-          password: [ null, [ Validators.required ] ]
-      });
-  }
-}   
+
+}
